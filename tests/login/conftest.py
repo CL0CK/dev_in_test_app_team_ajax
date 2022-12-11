4b977adb4a207cb84d8c127a91d6e68f19ec3902
CL0CK <humer7@icloud.com>
2022-12-11 15:21:21 +0200

    Test for Log In

--- tests/login/conftest.py
+++ tests/login/conftest.py
@@ -3,6 +3,8 @@ import pytest
 from framework.login_page import LoginPage
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def user_login_fixture(driver):
+    driver.launch_app()
     yield LoginPage(driver)
+    driver.close_app()
