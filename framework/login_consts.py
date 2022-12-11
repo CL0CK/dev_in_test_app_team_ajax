67f4b7bcc78345d861922499b73d1b141b7a8fbf
CL0CK <humer7@icloud.com>
2022-12-11 19:41:51 +0200

    Fix login test

--- framework/login_consts.py
+++ framework/login_consts.py
@@ -17,28 +17,5 @@ class LoginConsts:
               ".ViewGroup/android.view.ViewGroup/android.widget.LinearLayout[" \
               "2]/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ImageView"
 
-    ADD_HUB_LABEL = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout" \
-                    "/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget" \
-                    ".DrawerLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android" \
-                    ".widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[" \
-                    "1]/android.widget.TextView "
-
-    APP_SETTINGS_LABEL = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget" \
-                         ".FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout" \
-                         ".widget.DrawerLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView" \
-                         "/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[" \
-                         "2]/android.widget.TextView "
-
-    HELP_LABEL = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout" \
-                 "/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout" \
-                 "/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout" \
-                 "/android.widget.LinearLayout/android.widget.LinearLayout[3]/android.widget.TextView "
-
-    REPORT_A_PROBLEM_LABEL = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget" \
-                             ".FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx" \
-                             ".drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/androidx.recyclerview" \
-                             ".widget.RecyclerView/android.widget.LinearLayout/android.widget.LinearLayout/android" \
-                             ".widget.LinearLayout[4]/android.widget.TextView "
-
 
 
